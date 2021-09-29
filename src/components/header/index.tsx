385ec264757811898e0d7592deb1cb858db2dddb
kca2250 <2250katok@gmail.com>
2021-09-29 19:41:36 +0900

    SignUpボタンを表示させたくないページなのでpathがsignupならば非表示

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,20 +1,31 @@
-import { Box } from '@chakra-ui/layout';
 import React from 'react';
+import { Box, Text } from '@chakra-ui/layout';
+import { Link, useLocation } from 'react-router-dom';
 import Btn from '../Button';
 import LogoTitle from './LogoTitle';
 
 const Header: React.VFC = () => {
+	const location = useLocation();
+
 	return (
 		<header>
 			<Box
-				mt='3'
 				style={{
+					marginTop: '12px',
 					display: 'flex',
 					alignItems: 'center',
 					justifyContent: 'space-between',
 				}}>
 				<LogoTitle />
-				<Btn text='signup' size='sm' colorScheme='blue' variant='ghost' />
+				{location.pathname !== '/signup' ? (
+					<Link to='/signup'>
+						<Btn text='signup' size='sm' colorScheme='blue' variant='ghost' />
+					</Link>
+				) : (
+					<Text fontSize='sm' color='blackAlpha.600'>
+						新規登録画面
+					</Text>
+				)}
 			</Box>
 		</header>
 	);
