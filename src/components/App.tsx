1b78d55cef16e5a42f337282b390036fd6ca0a33
kca2250 <2250katok@gmail.com>
2021-09-29 19:38:54 +0900

    TopとSignUpのルーティングを追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.css';
 
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Container } from '@chakra-ui/react';
+
 import Header from './header';
+import Top from '../components/Pages/Top';
+import SignUp from '../components/Pages/SignUp';
 
 const App: React.VFC = () => {
 	return (
-		<Container>
-			<Header />
-		</Container>
+		<BrowserRouter>
+			<Container>
+				<Header />
+				<main style={{ margin: '15px 0px' }}>
+					{/* ルーティング */}
+					<Switch>
+						<Route exact path='/' component={Top} />
+						<Route path='/signup' component={SignUp} />
+					</Switch>
+				</main>
+			</Container>
+		</BrowserRouter>
 	);
 };
 
