b246194939c374d6962dd73426fd97855997eb25
kca2250 <2250katok@gmail.com>
2021-09-29 19:39:05 +0900

    軽微な修正

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
-import { Button } from '@chakra-ui/button';
 import React from 'react';
+import { Button } from '@chakra-ui/button';
 import { PropsType } from './type';
 
 const Btn: React.VFC<PropsType> = (props) => {
