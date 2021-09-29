43134a39208c6846afb012e496be6648f4c31f59
kca2250 <2250katok@gmail.com>
2021-09-29 19:40:26 +0900

    リンクを追加し、タイトル横に説明を追記

--- src/components/header/LogoTitle.tsx
+++ src/components/header/LogoTitle.tsx
@@ -1,22 +1,29 @@
 import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoSvg from '../../assets/book_review_app_logo.svg';
 
 const LogoTitle: React.VFC = () => {
 	return (
-		<Box
-			style={{
-				display: 'flex',
-				alignItems: 'center',
-			}}>
-			<img
-				src={LogoSvg}
-				alt='ロゴタイトル'
-				style={{ width: '30px', height: 'auto' }}
-			/>
-			<Text ml='1' as='h1' fontSize='xl' fontWeight='extrabold'>
-				B.R.A
-			</Text>
+		<Box>
+			<Link
+				to='/'
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+				}}>
+				<img
+					src={LogoSvg}
+					alt='ロゴタイトル'
+					style={{ width: '30px', height: 'auto' }}
+				/>
+				<Text ml='1' as='h1' fontSize='xl' fontWeight='extrabold'>
+					B.R.A
+				</Text>
+				<Text ml='3' fontSize='xs' color='blackAlpha.500'>
+					書籍レビューアプリ
+				</Text>
+			</Link>
 		</Box>
 	);
 };
