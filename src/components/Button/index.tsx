ec97a5852a919a782ac1c2b83b1deae8c5378dbb
kca2250 <2250katok@gmail.com>
2021-09-30 16:29:55 +0900

    propsにtypeを追加、typeは'button'か'submit'

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,9 +3,14 @@ import { Button } from '@chakra-ui/button';
 import { PropsType } from './type';
 
 const Btn: React.VFC<PropsType> = (props) => {
-	const { text, size, colorScheme, variant } = props;
+	const { type, text, size, colorScheme, variant } = props;
 	return (
-		<Button size={size} colorScheme={colorScheme} variant={variant}>
+		<Button
+			role='button'
+			type={type}
+			size={size}
+			colorScheme={colorScheme}
+			variant={variant}>
 			{text}
 		</Button>
 	);
--- src/components/Button/type.ts
+++ src/components/Button/type.ts
@@ -1,4 +1,5 @@
 export type PropsType = {
+	type: 'button' | 'submit';
 	text: string;
 	size: 'sm' | 'md';
 	colorScheme: 'blue' | 'red';
