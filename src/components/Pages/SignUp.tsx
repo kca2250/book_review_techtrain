5971849963d6ca5468cf20bc4956aee29551e2af
kca2250 <2250katok@gmail.com>
2021-09-30 16:47:52 +0900

    create SignUp page component

--- src/components/Pages/SignUp.tsx
+++ src/components/Pages/SignUp.tsx
@@ -1,10 +1,28 @@
+import { Text, Container } from '@chakra-ui/layout';
 import React from 'react';
+import { Link } from 'react-router-dom';
+import SignUpForm from '../Form/SignUpForm';
 
 const SignUp: React.VFC = () => {
 	return (
-		<div>
-			<p>this page is sign up page.</p>
-		</div>
+		<Container p='7' mt='8' maxW='sm'>
+			<Text
+				mb='4'
+				pb='3'
+				as='h2'
+				textAlign='center'
+				fontSize='lg'
+				fontWeight='bold'
+				borderBottom='1px solid #E6EDFA'>
+				新規登録
+			</Text>
+			<SignUpForm />
+			<Link to='/login'>
+				<Text mt='2' textAlign='right' fontSize='sm' color='blue.400'>
+					ログインはこちらから
+				</Text>
+			</Link>
+		</Container>
 	);
 };
 
