4a25a8027736fd7729b0b168b80c615cad61c1f5
kca2250 <2250katok@gmail.com>
2021-10-08 18:22:57 +0900

    各コンポーネントのインポートパスを修正

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import './App.css';
+import '../assets/css/App.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Container } from '@chakra-ui/react';
 
-import Header from './header';
-import Top from '../components/Pages/Top';
-import SignUp from '../components/Pages/SignUp';
+import Header from '../components/header';
+import Top from './Pages/Top';
+import SignUp from './Pages/SignUp';
 import Login from './Pages/Login';
 
 const App: React.VFC = () => {
