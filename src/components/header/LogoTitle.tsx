293934153e4c3fc662e1860659a63dd9d4ccf52e
kca2250 <2250katok@gmail.com>
2021-10-08 18:23:39 +0900

    タイトルのフォントサイズを変更しクラスネームを指定

--- src/components/header/LogoTitle.tsx
+++ src/components/header/LogoTitle.tsx
@@ -1,7 +1,7 @@
-import { Box, Text } from '@chakra-ui/react';
 import React from 'react';
+import { Box, Text } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import LogoSvg from '../../assets/book_review_app_logo.svg';
+import LogoSvg from '../../assets/img/book_review_app_logo.svg';
 
 const LogoTitle: React.VFC = () => {
 	return (
@@ -17,7 +17,12 @@ const LogoTitle: React.VFC = () => {
 					alt='ロゴタイトル'
 					style={{ width: '30px', height: 'auto' }}
 				/>
-				<Text ml='1' as='h1' fontSize='xl' fontWeight='extrabold'>
+				<Text
+					className='title'
+					ml='1'
+					as='h1'
+					fontSize='x-large'
+					fontWeight='extrabold'>
 					B.R.A
 				</Text>
 				<Text ml='3' fontSize='xs'>
