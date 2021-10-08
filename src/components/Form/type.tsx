06f63403a2fae90e78088dd3c7b3890b8a431280
kca2250 <2250katok@gmail.com>
2021-10-08 18:23:19 +0900

    型の名称を具体的な名前に変更

--- src/components/Form/type.tsx
+++ src/components/Form/type.tsx
@@ -1,4 +1,10 @@
-export type InputFormType = {
+export type SignInType = {
+	email: string;
+	password: string;
+};
+
+export type SignUpType = {
+	name: string;
 	email: string;
 	password: string;
 };
