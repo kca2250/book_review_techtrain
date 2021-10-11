4d7b5c46740bb77bd944c6824f1ebcf5e1a5d9f6
kca2250 <2250katok@gmail.com>
2021-10-11 18:55:13 +0900

    create useAuth custom hook

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import { useState } from 'react';
+import { useHistory } from 'react-router';
+import { SignUpType } from '../components/Form/type';
+import { useMessage } from './useMessage';
+
+export const useAuth = () => {
+	const history = useHistory();
+	const { successMessage, warningMessage, errorMessage } = useMessage();
+
+	const [isLoading, setIsLoading] = useState(false);
+
+	const signup = async (props: SignUpType): Promise<void> => {
+		const { name, email, password } = props;
+		setIsLoading(true);
+		await axios
+			.post('https://api-for-missions-and-railways.herokuapp.com/users', {
+				name,
+				email,
+				password,
+			})
+			.then((res) => {
+				console.log(res);
+				switch (res.status) {
+					case 200:
+						successMessage({ title: 'ユーザーを登録しました' });
+						history.push('/');
+						setIsLoading(false);
+						break;
+
+					case 500:
+						warningMessage({ title: 'サーバーで問題が起きました' });
+						setIsLoading(false);
+						break;
+
+					default:
+						errorMessage({ title: 'エラーが発生しました' });
+						setIsLoading(false);
+						break;
+				}
+			})
+			.catch((err) => {
+				console.log(err);
+				setIsLoading(false);
+			});
+	};
+
+	return { isLoading, signup };
+};
