687fe070ed29a0c645c2e595d49591f6edc3ccf2
kca2250 <2250katok@gmail.com>
2021-10-13 17:00:02 +0900

    ログインなどのユーザー関連のボタンを削除

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { Box } from '@chakra-ui/layout';
-import { Link, useLocation } from 'react-router-dom';
 
-import Btn from '../Button';
 import LogoTitle from './LogoTitle';
 
 const Header: React.VFC = () => {
-	const location = useLocation();
-
 	return (
 		<header>
 			<Box
@@ -16,23 +12,6 @@ const Header: React.VFC = () => {
 				alignItems='center'
 				justifyContent='space-between'>
 				<LogoTitle />
-				{location.pathname !== '/signup' ? (
-					location.pathname !== '/login' ? (
-						<Link to='/signup'>
-							<Btn
-								type='button'
-								text='signup'
-								size='sm'
-								colorScheme='blue'
-								variant='ghost'
-							/>
-						</Link>
-					) : (
-						''
-					)
-				) : (
-					''
-				)}
 			</Box>
 		</header>
 	);
