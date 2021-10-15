5ec93417ef1db470221672bb39b270c6520a1a66
kca2250 <2250katok@gmail.com>
2021-10-15 09:24:48 +0900

    AuthContenxtを適用しtokenと認証状態を保持させる

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,28 +2,30 @@ import React, { useState, useEffect } from 'react';
 import '../assets/css/App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Container } from '@chakra-ui/react';
-import { authContext } from '../contexts/authContext';
+import { AuthContext } from '../contexts/Auth/AuthContext';
 import Header from '../components/header';
 import Routing from '../Routes';
 
 const App: React.VFC = () => {
 	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+	const token: string | null = localStorage.getItem('auth_token');
 
 	useEffect(() => {
-		const token: string | null = localStorage.getItem('auth_token');
-		token ? setIsAuthenticated(true) : setIsAuthenticated(false);
-	}, [isAuthenticated]);
+		token && setIsAuthenticated(true);
+		console.log('render');
+	}, [isAuthenticated, token]);
 
 	return (
 		<BrowserRouter>
-			<authContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+			<AuthContext.Provider
+				value={{ token, isAuthenticated, setIsAuthenticated }}>
 				<Container>
 					<Header />
 					<main style={{ margin: '15px 0px' }}>
 						<Routing />
 					</main>
 				</Container>
-			</authContext.Provider>
+			</AuthContext.Provider>
 		</BrowserRouter>
 	);
 };
