b1a2af69bb6da8650d1308f50a9616540e8523e4
kca2250 <2250katok@gmail.com>
2021-10-15 09:25:56 +0900

    contextにtokenを追加

--- src/contexts/Auth/AuthContext.ts
+++ src/contexts/Auth/AuthContext.ts
@@ -1,7 +1,8 @@
 import React, { createContext } from 'react';
 
-export const authContext = createContext(
+export const AuthContext = createContext(
 	{} as {
+		token: string | null;
 		isAuthenticated: boolean;
 		setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
 	}
