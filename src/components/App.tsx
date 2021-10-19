b71c4cf32eba5d953d9ce360b9c11d6bf372aa06
kca2250 <2250katok@gmail.com>
2021-10-19 13:04:59 +0900

    fetchUserDataをインポートしuseEffect内に配置

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,21 +5,23 @@ import { Container } from '@chakra-ui/react';
 import { AuthContext } from '../contexts/Auth/AuthContext';
 import Header from '../components/header';
 import Routing from '../Routes';
+import { useAuth } from '../hooks/useAuth';
 
 const App: React.VFC = () => {
 	const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
-	const token = localStorage.getItem('auth_token') as string;
+	const token = localStorage.getItem('auth_token');
+	const { fetchUserData, userName } = useAuth();
 
 	useEffect(() => {
-		token && setIsAuthenticated(true);
-	}, [isAuthenticated, token]);
+		token ? setIsAuthenticated(true) : setIsAuthenticated(false);
+		fetchUserData();
+	}, [fetchUserData, isAuthenticated, token]);
 
 	return (
 		<BrowserRouter>
-			<AuthContext.Provider
-				value={{ token, isAuthenticated, setIsAuthenticated }}>
+			<AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
 				<Container>
-					<Header />
+					<Header userName={userName} />
 					<main style={{ margin: '15px 0px' }}>
 						<Routing />
 					</main>
