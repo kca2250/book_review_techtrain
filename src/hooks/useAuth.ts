ca2a568bf8b93ceeb3323ff044ef744f067c2caf
kca2250 <2250katok@gmail.com>
2021-10-19 13:03:27 +0900

    fetchUseDataを追加し、既存関数のthencatchをtrycatchに変更

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,60 +1,74 @@
 import axios from 'axios';
 import { useState } from 'react';
-import { useHistory } from 'react-router';
 import { SignInType, SignUpType } from '../components/Form/type';
 import { useMessage } from './useMessage';
 
 export const useAuth = () => {
-	const history = useHistory();
 	const { showMessage } = useMessage();
 	const [isLoading, setIsLoading] = useState(false);
+	const [userName, setUserName] = useState<string>('');
+	const token = localStorage.getItem('auth_token');
+
+	const fetchUserData = async () => {
+		setIsLoading(true);
+		await axios
+			.get('https://api-for-missions-and-railways.herokuapp.com/users', {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => {
+				setUserName(res.data.name);
+			})
+			.catch((err) => console.log(err));
+	};
 
 	const signup = async (props: SignUpType): Promise<void> => {
 		const { name, email, password } = props;
 		setIsLoading(true);
-
-		await axios
-			.post('https://api-for-missions-and-railways.herokuapp.com/users', {
+		const res = await axios.post(
+			'https://api-for-missions-and-railways.herokuapp.com/users',
+			{
 				name,
 				email,
 				password,
-			})
-			.then((res) => {
-				localStorage.setItem('auth_token', res.data.token);
-				showMessage({ title: 'ユーザーを登録しました', status: 'info' });
-			})
-			.catch((err) => {
-				console.log(err);
-				showMessage({ title: 'エラーが発生しました', status: 'error' });
-			})
-			.finally(() => {
-				setIsLoading(false);
-				history.push('/');
-			});
+			}
+		);
+
+		try {
+			localStorage.setItem('auth_token', res.data.token);
+			showMessage({ title: 'ユーザーを登録しました', status: 'info' });
+			return;
+		} catch (error) {
+			console.log(error);
+			showMessage({ title: 'エラーが発生しました', status: 'error' });
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
 	const login = async (props: SignInType): Promise<void> => {
 		const { email, password } = props;
 		setIsLoading(true);
-
-		await axios
-			.post('https://api-for-missions-and-railways.herokuapp.com/signin', {
+		const res = await axios.post(
+			'https://api-for-missions-and-railways.herokuapp.com/signin',
+			{
 				email,
 				password,
-			})
-			.then((res) => {
-				localStorage.setItem('auth_token', res.data.token);
-				showMessage({ title: 'ログインしました', status: 'info' });
-			})
-			.catch((err) => {
-				console.log(err);
-				showMessage({ title: 'エラーが発生しました', status: 'error' });
-			})
-			.finally(() => {
-				setIsLoading(false);
-				history.push('/');
-			});
+			}
+		);
+
+		try {
+			localStorage.setItem('auth_token', res.data.token);
+			showMessage({ title: 'ログインしました', status: 'info' });
+			return;
+		} catch (error) {
+			console.log(error);
+			showMessage({ title: 'エラーが発生しました', status: 'error' });
+		} finally {
+			setIsLoading(false);
+		}
 	};
 
-	return { isLoading, signup, login };
+	return { isLoading, userName, fetchUserData, signup, login };
 };
