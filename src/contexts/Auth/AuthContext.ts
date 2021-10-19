4104148b038284e89df0bd0600978e2d6b8e3d14
kca2250 <2250katok@gmail.com>
2021-10-19 13:02:24 +0900

    tokenを削除

--- src/contexts/Auth/AuthContext.ts
+++ src/contexts/Auth/AuthContext.ts
@@ -2,7 +2,6 @@ import React, { createContext } from 'react';
 
 export const AuthContext = createContext(
 	{} as {
-		token: string;
 		isAuthenticated: boolean;
 		setIsAuthenticated: React.Dispatch<React.SetStateAction<boolean>>;
 	}
