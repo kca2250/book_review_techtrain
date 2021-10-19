5af58315565453b4dd667c86f29c7527f7fcb34f
kca2250 <2250katok@gmail.com>
2021-10-19 13:03:43 +0900

    それぞれのルートにリダイレクト処理を追加

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,33 +1,24 @@
 import React, { useContext } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
-import { AuthContext } from '../contexts/Auth/AuthContext';
 
 import Login from '../components/Pages/Login';
 import SignUp from '../components/Pages/SignUp';
 import Top from '../components/Pages/Top';
+import { AuthContext } from '../contexts/Auth/AuthContext';
 
 const Routing: React.VFC = () => {
 	const { isAuthenticated } = useContext(AuthContext);
-
 	return (
 		<Switch>
-			<Route
-				exact
-				path='/'
-				render={() => (!isAuthenticated ? <Redirect to='/login' /> : <Top />)}
-			/>
-
-			<Route
-				path='/login'
-				render={() => (isAuthenticated ? <Redirect exact to='/' /> : <Login />)}
-			/>
-
-			<Route
-				path='/signup'
-				render={() =>
-					isAuthenticated ? <Redirect exact to='/' /> : <SignUp />
-				}
-			/>
+			<Route exact path='/'>
+				{isAuthenticated === true ? <Top /> : <Redirect push to='/signup' />}
+			</Route>
+			<Route path='/login'>
+				{isAuthenticated !== true ? <Login /> : <Redirect push to='/' />}
+			</Route>
+			<Route path='/signup'>
+				{isAuthenticated !== true ? <SignUp /> : <Redirect push to='/' />}
+			</Route>
 		</Switch>
 	);
 };
