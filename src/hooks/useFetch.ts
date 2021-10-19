f373c8343c8d21776c8a4c048d5412f20235e41a
kca2250 <2250katok@gmail.com>
2021-10-19 13:06:58 +0900

    書籍一覧を読み込む際に何かしらのエラーが出た場合にトップへ遷移するように

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 import { BookType } from '../components/Books/type';
 
-export const useFetch = (url: string, token: string) => {
+export const useFetch = (url: string, token: string | null) => {
+	const history = useHistory();
 	const [apiData, setApiData] = useState<BookType[]>([]);
 	const [error, setError] = useState<unknown>(null);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -16,17 +18,18 @@ export const useFetch = (url: string, token: string) => {
 						Authorization: `Bearer ${token}`,
 					},
 				});
-				const data: BookType[] = await res?.data;
+				const data: BookType[] = await res.data;
 				setApiData(data);
 				setIsLoading(false);
 			} catch (err) {
 				setError(err);
 				setIsLoading(false);
+				history.push('/login');
 			}
 		};
 
 		fetchData();
-	}, [token, url]);
+	}, [history, token, url]);
 
 	return { apiData, error, isLoading };
 };
