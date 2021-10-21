66a531b2d7bce30251c340d0fcd61429a8656c27
kca2250 <2250katok@gmail.com>
2021-10-21 16:02:18 +0900

    create editReview hook

--- src/hooks/useEdit.ts
+++ src/hooks/useEdit.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState } from 'react';
+import { ReviewType } from '../components/Books/type';
 import { useMessage } from './useMessage';
 
 export const useEdit = () => {
@@ -10,29 +11,66 @@ export const useEdit = () => {
 	const editUser = async (name: string) => {
 		setIsLoading(true);
 
-		await axios.put(
-			'https://api-for-missions-and-railways.herokuapp.com/users',
-			{
-				name,
-			},
-			{
-				headers: {
-					Authorization: `Bearer ${token}`,
+		await axios
+			.put(
+				'https://api-for-missions-and-railways.herokuapp.com/users',
+				{
+					name,
 				},
-			}
-		);
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			)
+			.then(() => {
+				showMessage({
+					title: `ユーザー名を ${name} に変更しました`,
+					status: 'info',
+				});
+			})
 
-		try {
-			setIsLoading(false);
-			showMessage({
-				title: `ユーザー名を ${name} に変更しました`,
-				status: 'info',
+			.catch((error) => {
+				console.log(error);
+				showMessage({ title: 'エラーが発生しました', status: 'error' });
+			})
+			.finally(() => {
+				setIsLoading(false);
 			});
-		} catch (error) {
-			console.log(error);
-			setIsLoading(false);
-		}
 	};
 
-	return { isLoading, editUser };
+	const editReview = async ({ id, title, url, detail, review }: ReviewType) => {
+		setIsLoading(true);
+
+		await axios
+			.put(
+				`https://api-for-missions-and-railways.herokuapp.com/books/${id}`,
+				{
+					title,
+					url,
+					detail,
+					review,
+				},
+				{
+					headers: {
+						Authorization: `Bearer ${token}`,
+					},
+				}
+			)
+			.then((res) => {
+				showMessage({
+					title: `レビュー内容を変更しました`,
+					status: 'info',
+				});
+			})
+			.catch((error) => {
+				console.log(error);
+				showMessage({ title: 'エラーが発生しました', status: 'error' });
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
+	};
+
+	return { isLoading, editUser, editReview };
 };
