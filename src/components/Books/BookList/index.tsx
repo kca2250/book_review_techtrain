b236048b2ed15974bf24319e359afa1d88cc39d8
kca2250 <2250katok@gmail.com>
2021-10-21 09:39:47 +0900

    トップページに新規作成ボタン配置

--- src/components/Books/BookList/index.tsx
+++ src/components/Books/BookList/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Progress, Grid } from '@chakra-ui/react';
-
 import { useFetch } from '../../../hooks/useFetch';
 import BookCard from '../BookCard';
+import Btn from '../../Button';
+import { useHistory } from 'react-router';
 
 const BookList: React.VFC = () => {
+	const history = useHistory();
 	const token = localStorage.getItem('auth_token');
 	const url: string =
 		'https://api-for-missions-and-railways.herokuapp.com/books';
@@ -13,23 +15,35 @@ const BookList: React.VFC = () => {
 
 	return (
 		<>
-			{isLoading && <Progress size='xs' isIndeterminate />}
-			<Grid overflow='hidden' templateColumns='repeat(4, 1fr)'>
-				{apiData.map((data, index) => {
-					return (
-						<BookCard
-							key={index}
-							id={data.id}
-							title={data.title}
-							detail={data.detail}
-							url={data.url}
-							review={data.review}
-							reviewer={data.reviewer}
-							isMine={data.isMine}
-						/>
-					);
-				})}
-			</Grid>
+			{isLoading ? (
+				<Progress size='xs' isIndeterminate />
+			) : (
+				<>
+					<Btn
+						text='新規作成'
+						type='button'
+						size='md'
+						colorScheme='blue'
+						variant='ghost'
+						onClick={() => history.push('/new')}
+					/>
+					<Grid overflow='hidden' templateColumns='repeat(4, 1fr)'>
+						{apiData.map((data, index) => {
+							return (
+								<BookCard
+									key={index}
+									id={data.id}
+									title={data.title}
+									detail={data.detail}
+									review={data.review}
+									reviewer={data.reviewer}
+									isMine={data.isMine}
+								/>
+							);
+						})}
+					</Grid>
+				</>
+			)}
 		</>
 	);
 };
