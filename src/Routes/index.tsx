a8e31ca303479c22ed65e2526132470eceb75418
kca2250 <2250katok@gmail.com>
2021-10-26 10:55:00 +0900

    Redirectを使わずにuseEffect hookで認証状態を監視するように

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,37 +1,30 @@
-import React, { useContext } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
+import React, { useContext, useEffect } from 'react';
+import { Route, Switch, useHistory } from 'react-router';
 import { AuthContext } from '../contexts/Auth/AuthContext';
 
 import Login from '../components/Pages/Login';
 import SignUp from '../components/Pages/SignUp';
-import Top from '../components/Pages/Top';
 import User from '../components/Pages/User';
+import BookList from '../components/Pages/BookList';
 import BookDetail from '../components/Pages/BookDetail';
 import CreateReview from '../components/Pages/CreateReview';
 import EditReviewForm from '../components/Form/EditReviewForm';
 
 const Routing: React.VFC = () => {
+	const history = useHistory();
 	const { isAuthenticated } = useContext(AuthContext);
+
+	useEffect(() => {
+		isAuthenticated === true ? history.push('/') : history.push('/signup');
+	}, [history, isAuthenticated]);
 
 	return (
 		<Switch>
-			<Route exact path='/'>
-				{isAuthenticated ? <Top /> : <Redirect push to='/signup' />}
-			</Route>
-			<Route path='/login'>
-				{!isAuthenticated ? <Login /> : <Redirect push to='/' />}
-			</Route>
-			<Route path='/signup'>
-				{!isAuthenticated ? <SignUp /> : <Redirect push to='/' />}
-			</Route>
-			<Route path='/profile'>
-				{isAuthenticated ? <User /> : <Redirect push to='/' />}
-			</Route>
-
-			<Route path='/detail/:id'>
-				{isAuthenticated ? <BookDetail /> : <Redirect push to='/' />}
-			</Route>
-
+			<Route exact path='/' component={BookList} />
+			<Route path='/login' component={Login} />
+			<Route path='/signup' component={SignUp} />
+			<Route path='/profile' component={User} />
+			<Route path='/detail/:id' component={BookDetail} />
 			<Route path='/new' component={CreateReview} />
 			<Route path='/edit/:id' component={EditReviewForm} />
 		</Switch>
