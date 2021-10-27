c58db5d1571f0bc8c18f67a3c8ec74b9904bd4f1
kca2250 <2250katok@gmail.com>
2021-10-27 20:48:59 +0900

    usefetchのfetchDataをこのコンポートに移動

--- src/components/Pages/BookList.tsx
+++ src/components/Pages/BookList.tsx
@@ -1,48 +1,81 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { Progress, Grid } from '@chakra-ui/react';
-import { useFetch } from '../../hooks/useFetch';
 import { Redirect, useHistory } from 'react-router';
 import BookCard from '../BookCard';
 import Btn from '../Button';
 import { AuthContext } from '../../contexts/Auth/AuthContext';
+import { BookType } from '../BookCard/type';
+import axios from 'axios';
 
 const BookList: React.VFC = () => {
-	const { isAuthenticated } = useContext(AuthContext);
 	const history = useHistory();
-	const { apiData, isLoading } = useFetch(
-		'https://api-for-missions-and-railways.herokuapp.com/books'
-	);
+	const { authToken, isAuthenticated, setIsAuthenticated } =
+		useContext(AuthContext);
+	const [apiData, setApiData] = useState<BookType[]>([]);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		setIsLoading(true);
+		const fetchData = async () => {
+			await axios
+				.get('https://api-for-missions-and-railways.herokuapp.com/books', {
+					headers: {
+						Authorization: `Bearer ${authToken}`,
+					},
+				})
+				.then((res) => {
+					setApiData(res.data);
+					console.log('render');
+				})
+				.catch((err) => {
+					if (axios.isAxiosError(err) && err.response?.status === 401) {
+						localStorage.removeItem('auth_token');
+						setIsAuthenticated(false);
+					}
+				})
+				.finally(() => {
+					setIsLoading(false);
+				});
+		};
+
+		fetchData();
+	}, [authToken, setIsAuthenticated]);
 
 	return (
 		<>
-			{!isAuthenticated && <Redirect to='/signup' />}
-			{isLoading ? (
-				<Progress size='xs' isIndeterminate />
+			{!isAuthenticated ? (
+				<Redirect to='/signup' />
 			) : (
 				<>
-					<Btn
-						text='新規作成'
-						type='button'
-						size='md'
-						colorScheme='blue'
-						variant='ghost'
-						onClick={() => history.push('/new')}
-					/>
-					<Grid overflow='hidden' templateColumns='repeat(4, 1fr)'>
-						{apiData.map((data, index) => {
-							return (
-								<BookCard
-									key={index}
-									id={data.id}
-									title={data.title}
-									detail={data.detail}
-									review={data.review}
-									reviewer={data.reviewer}
-									isMine={data.isMine}
-								/>
-							);
-						})}
-					</Grid>
+					{isLoading ? (
+						<Progress size='xs' isIndeterminate />
+					) : (
+						<>
+							<Btn
+								text='新規作成'
+								type='button'
+								size='md'
+								colorScheme='blue'
+								variant='ghost'
+								onClick={() => history.push('/new')}
+							/>
+							<Grid overflow='hidden' templateColumns='repeat(4, 1fr)'>
+								{apiData.map((data, index) => {
+									return (
+										<BookCard
+											key={index}
+											id={data.id}
+											title={data.title}
+											detail={data.detail}
+											review={data.review}
+											reviewer={data.reviewer}
+											isMine={data.isMine}
+										/>
+									);
+								})}
+							</Grid>
+						</>
+					)}
 				</>
 			)}
 		</>
